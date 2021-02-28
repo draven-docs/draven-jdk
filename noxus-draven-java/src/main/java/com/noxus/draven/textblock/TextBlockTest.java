ca33a0b9d5d2b5a80b0c542ef938980dc5f0d804
1064517852@qq.com <1064517852@qq.com>
2021-02-28 10:03:10 +0800

    Java-15-TextBlock

---
+++ noxus-draven-java/src/main/java/com/noxus/draven/textblock/TextBlockTest.java
@@ -0,0 +1,19 @@
+package com.noxus.draven.textblock;
+
+/**
+ * 〈功能概述〉<br>
+ *
+ * @className: TextBlockTest
+ * @package: com.noxus.draven.textblock
+ * @author: draven
+ * @date: 2021/2/28 09:16
+ */
+public class TextBlockTest {
+    public static void main(String[] args) {
+        String bock = """
+                \\
+                """;
+        System.out.println(bock);
+        System.out.println((String) null);
+    }
+}
